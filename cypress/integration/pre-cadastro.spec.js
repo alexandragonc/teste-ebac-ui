4eac6a2d470d6715ffb9dce30940cd36866c67d0
alexandragonc <alexandrag@outlook.com>
2021-10-27 20:04:16 -0300

    usando custom commands

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -25,7 +25,12 @@ describe('Funcionalidade Pré Cadastro', () => {
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
     });
 
+    it.only('Deve completar o pré cadastro com sucesso usando Comdandos customizados', () => {
+        let emailFaker2 = faker.internet.email()
+        cy.preCadastro(emailFaker2, 'senha!@forte', 'Maria', 'Alexandra')
 
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+    });
 
 
 
--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -14,7 +14,7 @@ describe('Funcionalidade Páginas de Produtos', () => {
             //.eq(3) //choose a product
             //.contains('Ariel Roll Sleeve Sweatshirt')
             .click()
-            //or cy.get('[class="product-block grid"]').first().click()
+        //or cy.get('[class="product-block grid"]').first().click()
     });
 
     it('Deve adicionar um produto ao carrinho', () => {
@@ -23,14 +23,20 @@ describe('Funcionalidade Páginas de Produtos', () => {
         cy.get('.product-block')
             .contains('Ariel Roll Sleeve Sweatshirt')
             .click()
-            cy.get('.button-variable-item-L').click()
-            cy.get('.button-variable-item-Purple').click()
-            cy.get('.input-text').clear().type(quantidade)
-            cy.get('.single_add_to_cart_button').click()
+        cy.get('.button-variable-item-L').click()
+        cy.get('.button-variable-item-Purple').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
 
-            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-            cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+    });
+    
+    it.only('Deve adicionar produtos ao carrinho - Usando Comando customizado', () => {
+        cy.addProdutos('Aero Daily Fitness Tee', 'M', 'Black', 2)
     });
 
-
+    it.only('Deve adicionar produtos ao carrinho - Usando Comando customizado', () => {
+        cy.addProdutos('Ariel Roll Sleeve Sweatshirt', 'XS', 'Green', 3)
+    });
 });
